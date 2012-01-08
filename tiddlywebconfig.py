429ce03f310d44ed7dc25ecb10a39c8e5afe1ac4
Selah Ben-Haim <selahb@gmail.com>
2012-01-08 08:49:22 -0600

    initial commit

---
+++ tiddlywebconfig.py
@@ -0,0 +1,9 @@
+# A basic configuration.
+# Run "pydoc tiddlyweb.config" for details on configuration items.
+
+config = {
+    'system_plugins': ['tiddlywebwiki'],
+    'secret': '0c575f9f78fe1ae7ae135c504fabf59004c0f915',
+    'twanager_plugins': ['tiddlywebwiki'],
+    'log_level': 'DEBUG',
+}
