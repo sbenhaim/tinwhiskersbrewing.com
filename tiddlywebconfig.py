de06e8951487dfee90d0c75e3e8a180f0068f5a2
Selah Ben-Haim <selahb@gmail.com>
2012-01-21 19:52:58 -0600

    plugin config, removing config for now

--- tiddlywebconfig.py
+++
@@ -1,9 +0,0 @@
-# A basic configuration.
-# Run "pydoc tiddlyweb.config" for details on configuration items.
-
-config = {
-    'system_plugins': ['tiddlywebwiki', 'tiddlywebplugins.socialusers', 'tiddlywebplugins.logout'],
-    'secret': '0c575f9f78fe1ae7ae135c504fabf59004c0f915',
-    'twanager_plugins': ['tiddlywebwiki'],
-    'log_level': 'DEBUG',
-}
